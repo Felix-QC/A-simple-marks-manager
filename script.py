0c96193df96d2114f7f84c4a69880c234a0fca08
Félix <51272629+Felix-QC@users.noreply.github.com>
2020-05-26 10:13:10 -0400

    Master

---
+++ script.py
@@ -0,0 +1,28 @@
+# Les librairies
+from inspect import getargspec
+
+# Calcul des note
+class Students:
+    nombre_eleve = 0
+    def __init__(self, name_):
+        Students.nombre_eleve += 1 
+        self.name_ = name_
+    def Matiere(self, french, math):
+        self.french = french
+        self.math = math
+        a = len(getargspec(Students.Matiere).args) - 1
+        calcul = (math + french) / a 
+        print(f"La note de, {self.name_}, est de: {abs(int(calcul))}%")
+        return calcul
+    
+# Liste des élvèves 
+felix_ = Students("Felix")
+charles_ = Students("Charles")
+pierre_alex_ = Students("Pierre-Alexandre")
+note_pa = pierre_alex_.Matiere(56, 67) # Les 2 nombres sont les notes reçus dans les différente matière au dessu
+note_felix = felix_.Matiere(90, 85) 
+note_charles = charles_.Matiere(60, 75)  
+
+# Print des résultat
+final_calcul = int((note_felix + note_charles + note_pa) / Students.nombre_eleve)
+print(f"\nLa moyenne du groupe est de : {final_calcul}%")
